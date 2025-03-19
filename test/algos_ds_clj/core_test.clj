57f1a27678a4bf8585e3da6e979911bae3da173a
diegoherrera262 <diegoherrera262@gmail.com>
2025-03-19 17:00:30 -0500

    [MISC] Condo files & test files fixes
    * Update condo files
    * Fix core.clj tests
    
    Signed-off-by: diegoherrera262@gmail.com <diegoherrera262@gmail.com>

--- test/algos_ds_clj/core_test.clj
+++ test/algos_ds_clj/core_test.clj
@@ -2,20 +2,6 @@
   (:require [clojure.test :refer :all]
             [algos-ds-clj.core :refer :all]))
 
-(deftest sample-test
-  (testing "creating a new test for clojure"
-    (is (= 1 2))))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (deftest some-test
   (testing "New tests"
     (is (= 1 1))))
-
-(defn my-sample
-  "Some sample fun that receives some args to produce an output"
-  [& args]
-  (count args))
-
